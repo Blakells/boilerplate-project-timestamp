2c6cad6e3203c681e1f66d530857af52bca0f104
Blakells <blake.bailey09@gmail.com>
2020-02-25 09:21:53 -0500

    Timestamp API microservice

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/timestamp/', (req, res, next) => {
+console.log('boobs');
+})
 
 
 // listen for requests :)
